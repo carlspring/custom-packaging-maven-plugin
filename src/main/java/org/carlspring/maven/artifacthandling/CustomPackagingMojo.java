92b8aa9fe493b2e851235c83df866ae73c9028d7
Martin Todorov <carlspring@gmail.com>
2013-03-28 14:36:56 +0200

    Added the option to specify a path to the artifact.

--- src/main/java/org/carlspring/maven/artifacthandling/CustomPackagingMojo.java
+++ src/main/java/org/carlspring/maven/artifacthandling/CustomPackagingMojo.java
@@ -8,10 +8,11 @@ import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.MavenProjectHelper;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * @author mtodorov
- * @goal   attach-artifact
+ * @goal attach-artifact
  */
 public class CustomPackagingMojo
         extends AbstractMojo
@@ -37,22 +38,62 @@ public class CustomPackagingMojo
      */
     public String basedir;
 
+    /**
+     * @parameter property="artifactPath"
+     */
+    public String artifactPath;
+
 
     @Override
     public void execute()
             throws MojoExecutionException, MojoFailureException
     {
-        File file = new File(basedir +"/target/",
-                             project.getArtifactId() +"-" +
-                             project.getVersion() + "." +
-                             project.getPackaging());
+        File file;
+        if (artifactPath != null)
+        {
+            try
+            {
+                file = new File(artifactPath).getCanonicalFile();
+            }
+            catch (IOException e)
+            {
+                throw new MojoExecutionException("Could not find artifact file!", e);
+            }
+        }
+        else
+        {
+            file = new File(basedir + "/target/",
+                            project.getArtifactId() + "-" +
+                            project.getVersion() + "." +
+                            project.getPackaging());
+        }
 
         Artifact artifact = project.getArtifact();
         artifact.setFile(file);
 
         project.setArtifact(artifact);
 
-        getLog().info("Attached " + artifact.toString() +".");
+        getLog().info("Attached " + artifact.toString() + ".");
+    }
+
+    public String getBasedir()
+    {
+        return basedir;
+    }
+
+    public void setBasedir(String basedir)
+    {
+        this.basedir = basedir;
+    }
+
+    public String getArtifactPath()
+    {
+        return artifactPath;
+    }
+
+    public void setArtifactPath(String artifactPath)
+    {
+        this.artifactPath = artifactPath;
     }
 
 }
